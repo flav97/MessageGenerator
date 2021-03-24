e922106c402a53ea7c94a004e5e633b7382be135
flav97 <53238145+flav97@users.noreply.github.com>
2021-03-24 21:12:10 +0000

    Js functionality

---
+++ app.js
@@ -0,0 +1,33 @@
+let finalMessage = [];
+
+const messages = {
+    zodiac: ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius ','Pisces'],
+    personality: ['warm','enthusiastic','creative','adaptable','original'],
+    expected: ['Find your soulmate','Progress further in your career','Be traveling to new places','Make new connections'],
+    pulhac: ['Progress further in your career','Be traveling to new places','Make new connections']
+    
+};
+
+const randomMsg = num =>{
+return Math.floor(Math.random() * num)
+};
+
+for(let prop in messages){
+    let index = randomMsg(messages[prop].length);
+ 
+    switch(prop){
+        case 'zodiac':
+        finalMessage.push(`Your zodiac sign is ${messages[prop][index]}.`);
+        break;
+        case 'personality':
+        finalMessage.push(`You are very ${messages[prop][index]}.`);
+        break;
+        case 'expected':
+        finalMessage.push(`It is expected that you will: ${messages[prop][index]}`) ;
+        break;         
+    }
+}
+
+let finalOutput = finalMessage.join("\n");
+
+console.log(finalOutput);
